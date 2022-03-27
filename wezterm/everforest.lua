455fbd9a590320497a6c2e32de9194f3c9374453
kije motan <85043033+kijemotantakalu@users.noreply.github.com>
2022-03-27 22:52:39 +0200

    Add files via upload

---
+++ wezterm/everforest.lua
@@ -0,0 +1,12 @@
+return {
+	foreground = "d3c6aa",
+	background = "2f383e",
+	cursor_bg = "7fbbb3",
+	cursor_fg = "2f383e",
+	cursor_border = "7fbbb3",
+	selection_fg = "3c474d",
+	selection_bg = "525c62",
+	scrollbar_thumb = "d8cacc",
+	ansi = {"#4b565c","#e67e80","#a7c080","#dbbc7f","#7fbbb3","#d699b6","#83c092","#d3c6aa"},
+	brights = {"#4b565c","#e67e80","#a7c080","#dbbc7f","#7fbbb3","#d699b6","#83c092","#d3c6aa"}
+}
